d70711de2cc4609694cd54e54ee3a1a372c7d40d
israoun11 <israoun55@gmail.com>
2026-09-20 16:23:01 +0100

    Delete backend directory

--- backend/src/routes/user.routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { protect } from "../middleware/auth.middleware.js";
-import { getMe } from "../controllers/user.controller.js";
-
-export const userRouter = Router();
-
-userRouter.get("/me", protect, getMe);
